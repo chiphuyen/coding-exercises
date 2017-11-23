1a337963839ab68414242a440964f1bf629bf095
kgashok <kgashok@users.noreply.github.com>
2017-11-23 08:59:18 +0530

    fixed errors in fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,15 +6,15 @@ of both three and five print “FizzBuzz”.
 '''
 def fizzbuzz():
 	results = ''
-	for i in range(100):
-		if (i + 1) % 15 == 0:
-			results += 'fizzbuzz'
-		elif (i + 1) % 3 == 0:
-			results += 'fizz'
-		elif (i + 1) % 5 == 0:
-			results += 'buzz'
+	for i in range(1, 101):
+		if (i) % 15 == 0:
+			results += 'fizzbuzz '
+		elif (i) % 3 == 0:
+			results += 'fizz '
+		elif (i) % 5 == 0:
+			results += 'buzz '
 		else:
-			results += ' '
+			results += str(i)+' '
 	return results
 
 print(fizzbuzz())
